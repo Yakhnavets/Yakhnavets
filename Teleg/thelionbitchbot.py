bb64faedd7ff968a6c622de0fd230428e0e4b344
Yakhnavets <consultant.yahnovets@gmail.com>
2021-09-22 22:15:20 +0300

    +

---
+++ Teleg/thelionbitchbot.py
@@ -0,0 +1,12 @@
+import telebot
+bot = telebot.TeleBot('2022444664:AAFBZpdfjFs1CDyEmh7RM9APh19uOSlOiGE')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+  if message.text == "Нахуй":
+      bot.send_message(message.from_user.id, "Привет, сам иди.")
+  elif message.text == "Писька":
+      bot.send_message(message.from_user.id, "Писюн")
+  else:
+      bot.send_message(message.from_user.id, "Я не понимаю о чем ты")
+      
+bot.polling(none_stop=True, interval=0)
